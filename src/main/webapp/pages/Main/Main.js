03a46e932799f5070423ddd3384523751c741953
system null <systemuser@wavemaker.com>
2015-05-18 13:47:42 +0000

    New files added - [artist_image.smd,list_genres_API.json,echonest_API.json,artist_video_API.json,profile_genre.spring.xml,artist_video_apiDocument.json,artist_id_name_apiDocument.json,artist_songs.spring.xml,artist_id_name.smd,artist_songs_API.json,artist_video.smd,servicedef.xml,servicedef.xml,artist_image_API.json,servicedef.xml,servicedef.xml,servicedef.xml,artist_songs.smd,profile_genre_API.json,profile_genre.smd,artist_id_name_API.json,artist_url_API.json,list_genres.spring.xml,artist_image_apiDocument.json,echonest_apiDocument.json,artist_image.spring.xml,list_genres_apiDocument.json,list_genres.smd,echonest.spring.xml,servicedef.xml,artist_video.spring.xml,artist_id_name.spring.xml,profile_genre_apiDocument.json,echonest.smd,servicedef.xml,artist_url.smd,artist_url.spring.xml,artist_songs_apiDocument.json,servicedef.xml,artist_url_apiDocument.json] Files modified - [project-managers.xml,Main.html,Main.js,login.html,project-services.xml,index.html,.wmproject.properties,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,15 +1,17 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action with the variables inside this block(on-page-load) */
-    $scope.onPageVariablesReady = function () {
+    $scope.onPageVariablesReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. $scope.Variables.staticVariable1.getData()
          */
+        debugger
+        //   $scope.Variables.Echonest_list_genresInvoke.update();
     };
     /* perform any action with widgets inside this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. $scope.Widgets.byId(), $scope.Widgets.byName()or access widgets by $scope.Widgets.widgetName
